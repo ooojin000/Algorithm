ec3123963ee93ded55e52bce5129c1d2e3ce73f9
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2024-09-05 08:27:41 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a % 2 == 1 | b % 2 == 1) {
+            answer = 2 * (a + b);
+        } 
+        
+        if(a % 2 == 1 & b % 2 == 1) {
+            answer = (a * a) + (b * b);
+        }
+        
+        if(a % 2 == 0 & b % 2 == 0) {
+            if(a > b) {
+                answer = a - b;
+            } else {
+                answer = b - a;
+            }
+        }
+        
+        return answer;
+    }
+}
