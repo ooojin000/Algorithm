bc386147f24b9f5533ee230f834fb66778f7938e
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-07-11 12:17:17 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 412 ms, Memory: 28208 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String line = sc.nextLine();
+            if (line.equals(".")) {
+                break;
+            }
+
+            if (isBalanced(line)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+
+    public static boolean isBalanced(String line) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : line.toCharArray()) {
+            if (ch == '(' || ch == '[') {
+                stack.push(ch);
+            } else if (ch ==')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
