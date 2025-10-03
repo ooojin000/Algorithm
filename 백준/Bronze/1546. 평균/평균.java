d09c7c5d73d346c2e028b538de2051bd4dce42d3
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-10-04 07:55:53 +0900

    [Bronze I] Title: 평균, Time: 228 ms, Memory: 21016 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            sum += arr[i];
+        }
+
+        System.out.println(sum * 100.0 / max / n);
+    }
+}
