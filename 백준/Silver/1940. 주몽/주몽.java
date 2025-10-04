43893c5024bdffcdf8729264fe869bad278a0a5d
ooojin000 <142341367+ooojin000@users.noreply.github.com>
2025-10-04 16:59:25 +0900

    [Silver IV] Title: 주몽, Time: 404 ms, Memory: 34204 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int cnt = 0;
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j) {
+            if (arr[i] + arr[j] == m) {
+                cnt++;
+                i++;
+                j--;
+            } else if (arr[i] + arr[j] < m) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
